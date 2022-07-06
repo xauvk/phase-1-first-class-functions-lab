4047f3fa84ce2c8b23e22beb48187c09af90dcfe
xauvk <xauvkubalexanderyang@live.com>
2022-07-06 14:29:38 -0400

    commit tree 7c2e8ee59470b80c58982d3f5db2c148cff5bbac

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (a) => {
+    a = [first, second]
+    return [first, second]
+}
+
+const returnLastTwoDrivers = (a) => {
+    a = [first, second]
+    return [first, second]
+}
